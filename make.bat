42f3121ed7379d6732c5c4a884bdb19484cec5a0
Atduyar <atduyar@gmail.com>
2021-07-09 21:28:57 +0300

    tamam

--- make.bat
+++ make.bat
@@ -2,5 +2,7 @@ git add .\blogView.html
 git add .\index.html
 git add .\css\blogView.css
 git add .\blogs.html
+git add .\blogs.css
+git add .\make.bat
 git commit -m "tamam"
 git push -u origin master
