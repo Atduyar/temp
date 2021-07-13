065f16eed13db5d342effb3c3780c50da1ccba0a
Atduyar <atduyar@gmail.com>
2021-07-14 02:38:38 +0300

    baya güzel oldu

--- htmlCreator/main.cpp
+++ htmlCreator/main.cpp
@@ -2,22 +2,37 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <Windows.h>
 using namespace std;
-/*
-in
-out
-
-
-
-
-*/
 
 int gc;
 int gci;
 char **gar;
 string exitFile = "";
+string exitLink = "";
+string exitScript = "";
 
+#ifdef _WIN32
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+const string innerP = "┼───";
+int routingColor = 14;
+#else
 const string innerP = "\t";
+#endif
+const string cBeforeName = "";
+const string cEndOfName = "";//" Finish.";
+
+
+const string cStandart = "<atd-";
+const string cRouter = "<atdr/>";
+const string cLinkStart = "<atdl>";
+const string cLinkEnd = "</atdl>";
+const string cScriptStart = "<atds>";
+const string cScriptEnd = "</atds>";
+const string cLinkEx = "<atdl!/>";
+const string cScriptEx = "<atdl!/>";
+
+
 
 void getHtml(string filePath, string tree){
 
@@ -28,9 +43,9 @@ void getHtml(string filePath, string tree){
     {
         while ( getline (myfile,line) )
         {
-            if (line.find("<atd-") != std::string::npos) {
+            if (line.find(cStandart) != std::string::npos) {
                 string name = "";
-                for (size_t i = line.find("<atd-")+5; i < line.size(); ++i)
+                for (size_t i = (line.find(cStandart)+cStandart.size()); i < line.size(); ++i)
                 {
                     if (line[i] == '/' && line[i+1] == '>')
                     {
@@ -39,7 +54,7 @@ void getHtml(string filePath, string tree){
                     name += line[i];
                 }
 
-                cout << tree << "-" << name << ": " << '\n';
+                cout << tree << cBeforeName << name << ": " << '\n';
                 
                 string upperName = "";
                 for (auto & c: name) upperName += toupper(c);
@@ -47,7 +62,7 @@ void getHtml(string filePath, string tree){
                 getHtml(name, tree + innerP);
                 exitFile += "<!-- STOP  " + upperName + " -->\n";
             }
-            else if(line.find("<atdr/>") != std::string::npos){
+            else if(line.find(cRouter) != std::string::npos){
                 string name = "";
                 if (gc >= gci)
                 {
@@ -59,21 +74,62 @@ void getHtml(string filePath, string tree){
                 }
                 
 
-                cout << tree << " -" << name << ": " << '\n';
-                
+                cout << tree << cBeforeName;
+                #ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, routingColor);
+                --routingColor;
+                #endif
+                cout << name << ": " << '\n';
+                #ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, 15);
+                #endif
+
                 string upperName = "";
                 for (auto & c: name) upperName += toupper(c);
                 exitFile += "<!-- START " + upperName + " R-->\n";
                 getHtml(name, tree + innerP);
                 exitFile += "<!-- STOP  " + upperName + " R-->\n";
             }
+            else if(line.find(cLinkStart) != std::string::npos){
+                string name = "";
+                bool flag = true;
+                
+                while (flag){
+                    getline(myfile,name);
+                    if(name.find(cLinkEnd) != std::string::npos){
+                        flag = false;
+                        break;
+                    }
+                    else{
+                        exitLink += name + "\n";
+                    }
+                }
+                
+            }
+            else if(line.find(cScriptStart) != std::string::npos){
+                string name = "";
+                bool flag = true;
+                
+                while (flag){
+                    getline(myfile,name);
+                    if(name.find(cScriptEnd) != std::string::npos){
+                        flag = false;
+                        break;
+                    }
+                    else{
+                        exitScript += name + "\n";
+                    }
+                }
+                
+            }
             else{
                 exitFile += line + "\n";
             }
-            
         }
         myfile.close();
-        cout << tree << "Finish." << endl; 
+        if(cEndOfName != "")
+            cout << tree.substr(0, (tree.size()-innerP.size())) << cEndOfName << endl; 
+
     }
     else cout << tree << "Unable to open file"; 
 
@@ -90,22 +146,56 @@ void setHtml(string name){
     else cout << "Dosya olusturulamadı!" << endl;
 }
 
-void getLink(){
+void getLink(){//<atds!/>
+    if (exitFile.find(cLinkEx) != std::string::npos) {
+        int i = exitFile.find(cLinkEx);
+        string temp = exitFile;
+        exitFile = temp.substr(0,i);
+        exitFile += "<!-- START LINK -->\n";
+        exitFile += exitLink;
+        exitFile += "<!-- STOP  LINK -->\n";
+        exitFile += temp.substr ((i+cLinkEx.size()), temp.size());
+    }
+    else{
+        cout << "Ekstra link yok" << endl;
+    }
+}
 
+void getScript(){//<atds!/>
+    if (exitFile.find(cScriptEx) != std::string::npos) {
+        int i = exitFile.find(cScriptEx);
+        string temp = exitFile;
+        exitFile = temp.substr(0,i);
+        exitFile += "<!-- START SCRIPT -->\n";
+        exitFile += exitScript;
+        exitFile += "<!-- STOP  SCRIPT -->\n";
+        exitFile += temp.substr ((i+cScriptEx.size()), temp.size());
+    }
+    else{
+        cout << "Ekstra Script yok" << endl;
+    }
 }
 
 int main(int argc, char** argv) {
-    cout << "Start: HtmlCreator - " << argc << " - " << argv[1] << endl;
+    
+    #ifdef _WIN32
+        SetConsoleOutputCP(CP_UTF8);
+    #endif
+    
+    cout << endl << "Start: HtmlCreator - " << argv[1] <<  " - " << argc << endl;
+    
     gc = argc;
     gci = 2;
     gar = argv;
 
-    getHtml("main","");
-
+    getHtml("main", innerP);
+    cout << "End: " << argv[1] << endl;
     getLink();
-
+    getScript();
+    
     setHtml(argv[1]);
 
+    //cout << endl << endl << exitLink << endl << endl;
     //cout << endl << endl << exitFile << endl << endl;
     cout << endl;
     return 0;
