a31eb739db0dbdb59fe87c14a18a51bb6b1d4811
Atduyar <atduyar@gmail.com>
2021-07-27 19:35:25 +0300

    tamam

--- js/login.js
+++ js/login.js
@@ -26,7 +26,7 @@ function login() {
     var loginFormInputs = document.getElementById("loginForm").getElementsByTagName("input");
 
     api.loginBody = { emailornickname: loginFormInputs[0].value, password: loginFormInputs[1].value }
-    userAction();
+    api.userAction();
 
     console.log("anan");
 }
