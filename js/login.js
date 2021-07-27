7d8fb4d5d74449078ceb3e41a03f29794166994f
Atduyar <atduyar@gmail.com>
2021-07-27 19:16:00 +0300

    tamam

--- js/login.js
+++ js/login.js
@@ -19,6 +19,19 @@ signInButtonM.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
 
+const userAction = async() => {
+    const response = await fetch('https://api.atduyar.com/api/auth/login', {
+        method: 'POST',
+        body: { emailornickname: email, password: pass },
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+    console.log(myJson);
+}
+
 function login() {
     var email = document.getElementById("loginForm").getElementsByTagName("input");
     var pass = email[1].value;
