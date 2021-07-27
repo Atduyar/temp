34924c9f61cf05c7982b934c1761aa5c7d435056
Atduyar <atduyar@gmail.com>
2021-07-27 19:28:14 +0300

    tamam

--- js/login.js
+++ js/login.js
@@ -19,25 +19,26 @@ signInButtonM.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
 
-const userAction = async() => {
-        const response = await fetch('https://api.atduyar.com/api/auth/login', {
-                method: 'POST',
-                body: JSON.stringify(loginBody),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-            const myJson = await response.json(); //extract JSON from the http response
-            // do something with myJson
-            console.log(loginBody); console.log(myJson);
+const Login = async() => {
+    const response = await fetch('https://api.atduyar.com/api/auth/login', {
+        method: 'POST',
+        body: JSON.stringify(loginBody),
+        headers: {
+            'Content-Type': 'application/json'
         }
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+    console.log(loginBody);
+    console.log(myJson);
+}
 
-        var loginBody = null;
+var loginBody = null;
 
-        function login() {
-            var email = document.getElementById("loginForm").getElementsByTagName("input");
-            var pass = email[1].value;
-            email = email[0].value;
-            loginBody = { emailornickname: email, password: pass };
-            userAction();
-        }
+function login() {
+    var email = document.getElementById("loginForm").getElementsByTagName("input");
+    var pass = email[1].value;
+    email = email[0].value;
+    loginBody = { emailornickname: email, password: pass };
+    userAction();
+}
