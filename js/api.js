26668b5c1535b2fe313aeedce92ee55c10ad7aea
Atduyar <atduyar@gmail.com>
2021-07-27 19:32:27 +0300

    Co-authored-by: Zafer Çetin <igzafer0@users.noreply.github.com>

---
+++ js/api.js
@@ -0,0 +1,21 @@
+class ApiAuth {
+    constructor(loginBody) {
+        this.loginBody = loginBody;
+    }
+    constructor() {
+
+    }
+    static Login = async() => {
+        const response = await fetch('https://api.atduyar.com/api/auth/login', {
+            method: 'POST',
+            body: JSON.stringify(loginBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const myJson = await response.json(); //extract JSON from the http response
+        // do something with myJson
+        console.log(loginBody);
+        console.log(myJson);
+    }
+}
