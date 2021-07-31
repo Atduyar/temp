7a1b4c8da79ad72f08473d329acf4ddcd360c39e
Atduyar <atduyar@gmail.com>
2021-07-31 15:39:25 +0300

    tamam

--- js/navbar.js
+++ js/navbar.js
@@ -5,8 +5,10 @@ const capitalizeFirstLetter = ([first, ...rest], locale = navigator.language) =>
     first.toLocaleUpperCase(locale) + rest.join('')
 
 function openNav() {
-    mySidenav.style.backgroundColor = "rgba(17,17,17,1)";
-    mySidenav.style.display = " block!important;"
+    
+    mySidenav.style = "display block!important; background-color:rgba(17,17,17,1)";
+    // mySidenav.style.backgroundColor = "rgba(17,17,17,1)";
+    // mySidenav.style.display = " block!important;"
     myMask.style.backgroundColor = "rgba(0,0,0,0.4)";
     myMask.style = " display:block!important;";
 }
