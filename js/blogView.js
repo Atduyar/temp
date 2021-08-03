8b789f0f030b023c167551db542cd8721457c9f6
Atduyar <atduyar@gmail.com>
2021-08-03 22:15:06 +0300

    tamam

--- js/blogView.js
+++ js/blogView.js
@@ -7,7 +7,14 @@ function setBlogDetail(b){
     var titleAut = title.getElementsByClassName("p-publish-title")[0];
     var blogDate = new Date(b.blogDate).toDateString();
     var blogTitle = title.getElementsByClassName("p-main-title")[0];
-
+    var blogTitle = title.getElementsByClassName("p-main-title")[0];
+    var tags = document.getElementsByClassName("kategori-bar")[0];
+    var img = document.getElementsByClassName("top-bar")[0].getElementsByClassName("img-div")[0].getElementsByTagName("img");
+    img.src = b.blogTitlePhotoUrl;
+    tags.innerHTML = "";
+    for(var i = 0;i<b.blogTags.length;i++){
+        tags.innerHTML += `<a href="/category/${b.blogTags[i].id}" class="p-kategori inactive c-p td-n">${b.blogTags[i].name}</a>`;
+    }
     blogTitle.innerHTML = b.blogTitle;
     titleAut.innerHTML = blogDate +` tarihinde <a class='inactive c-p td-u'>${b.authorSummary.authorName}</a> tarafından yazıldı.`;
 }
