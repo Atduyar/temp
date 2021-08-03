f7d57fc8bb7f46d5a81a6b7c427cea0008ff65cc
Atduyar <atduyar@gmail.com>
2021-08-03 21:11:59 +0300

    tamam

--- js/blogView.js
+++ js/blogView.js
@@ -2,22 +2,32 @@ var apiBlogDetail = new ApiAuth();
 const urlParams = new URLSearchParams(window.location.search);
 const BlogId = urlParams.get('id');
 
-function setBlogDetail(){}
+function setBlogDetail(b){
+    var title = document.getElementsByClassName("title-bar")[0];
+    var titleAut = title.getElementsByClassName("p-publish-title")[0];
+    var blogDate = new Date(b.blogDate).toDateString();
 
+    titleAut.innerHTML = blogDate +` <a class='inactive c-p td-u'>${b.authorSummary.authorName}</a> tarafından yazıldı.`;
+}
 
 getBlogDetail(BlogId);
 function getBlogDetail(id){
     apiBlogDetail.resultFunction = (t)=>{
         apiBlogDetail.resultFunction = (b)=>{
             console.log(b);
+            setBlogDetail(b);
         }
         apiBlogDetail.GetAuth("blogs/getBlog?id="+id, t.token);
     }
+    apiBlogDetail.resultUnAuthFunction = (r)=>{
+        apiBlogDetail.resultFunction = (b)=>{
+            console.log(b);
+            setBlogDetail(b);
+        }
+        apiBlogDetail.GetAuth("blogs/getBlogGuest?id="+id);
+    }
     apiBlogDetail.resultErrFunction = (err)=>{
         console.log(err);
-    }
-    apiBlogDetail.resultUnAuthFunction = (r)=>{
-        console.log(r);
     }
     ApiAuth.GetToken(apiBlogDetail);
 }
