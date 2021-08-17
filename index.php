aa252e1577de114338cd2e5410666988fe29853b
Atduyar <atduyar@gmail.com>
2021-08-17 17:29:24 +0300

    tamam

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-    // echo "Hello World : ";
-    
     $x = explode("/", $_SERVER["REQUEST_URI"]);
     // print_r($x);
     // $y = __DIR__ . '/' . $x[1] . '.html?id=' . (count($arr) > 2 ? $x[2] : '') ;
