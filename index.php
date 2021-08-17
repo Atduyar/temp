ae085cd21bd7be109a43e11b34a0801c360e8730
Atduyar <atduyar@gmail.com>
2021-08-17 12:20:23 +0300

    tamam

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
     echo "Hello World";
-    echo $1;
+    
+    $params = explode( "/", $_GET['url'] );
+    for($i = 0; $i < count($params); $i+=2) {
+
+      echo $params[$i] ." has value: ". $params[$i+1] ."<br />";
+
+    }
 ?>
