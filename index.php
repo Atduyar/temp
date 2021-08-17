6ad6d528a982684adc44a9be6eaaaadfd5471de1
Atduyar <atduyar@gmail.com>
2021-08-17 21:19:51 +0300

    tamam

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
              'setParam("' . $x[2] . '");',
              '</script>';
     }
-    elseif(count($x) > 1){
+    elseif(count($x) > 1 and $x[1] != ""){
         $y = __DIR__ . '/' . $x[1] . '.html';
         require $y;
     }else {
