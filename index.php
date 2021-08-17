7cd7da9d7b387c4c624ba81f005bb069d4643a06
Atduyar <atduyar@gmail.com>
2021-08-17 12:52:05 +0300

    tamam

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     
     $x = explode("/", $_SERVER["REQUEST_URI"]);
     print_r($x);
-    echo  __DIR__ . '/' . $x[1] . '.html?id=' . (count($arr) > 2 ? $x[2] : '') ;
+    $y = __DIR__ . '/' . $x[1] . '.html?id=' . (count($arr) > 2 ? $x[2] : '') ;
+    echo $y;
     require __DIR__ . '/' . $x[1] . '.html?id=' . (count($arr) > 2 ? $x[2] : '') ;
 ?>
