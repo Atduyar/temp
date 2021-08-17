d1e2cf524fd2e919574978cb814a923ae0ed0012
Atduyar <atduyar@gmail.com>
2021-08-17 20:54:50 +0300

    tamam

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     // print_r($x);
     // $y = __DIR__ . '/' . $x[1] . '.html?id=' . (count($arr) > 2 ? $x[2] : '') ;
     
-    if ($count($x) > 2){
+    if (count($x) > 2){
         $y = __DIR__ . '/' . $x[1] . '.html';
         require $y;
         echo '<script type="text/javascript">',
