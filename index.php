9196c1c0a32d059f70c32ddb079d2baa7702f1ef
Atduyar <atduyar@gmail.com>
2021-08-17 13:10:15 +0300

    tamam

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-    echo "Hello World";
+    echo "Hello World : ";
     
     $x = explode("/", $_SERVER["REQUEST_URI"]);
     // print_r($x);
     // $y = __DIR__ . '/' . $x[1] . '.html?id=' . (count($arr) > 2 ? $x[2] : '') ;
     $y = __DIR__ . '/' . $x[1] . '.html?id=';
     echo $y;
-    // require __DIR__ . '/' . $x[1] . '.html?id=' . (count($arr) > 2 ? $x[2] : '') ;
+    require $y;
 ?>
