cdc7ef78a97935de3cd0f154f4ac486f56e5a2ab
Atduyar <atduyar@gmail.com>
2021-08-19 20:19:00 +0300

    tamam

--- js/navbar.js
+++ js/navbar.js
@@ -29,8 +29,8 @@ function getUser() {
                 document.getElementById("mySidenav").getElementsByClassName("menu-items")[0].classList.add("auth");
                 document.getElementById("nav-bar-menu").classList.add("auth");
 
-                document.getElementById("mySidenav").getElementsByClassName("menu-items")[0].classList.add("auth");
-                document.getElementById("nav-bar-menu").getElementsByClassName("profil")[0].getElementsByClassName("profil")[0].href = "/user/" + u.nickname;
+                document.getElementById("mySidenav").getElementsByClassName("menu-items")[0].getElementsByClassName("profil")[0].href = "/user/" + u.nickname;
+                document.getElementById("nav-bar-menu").getElementsByClassName("profil")[0].href = "/user/" + u.nickname;
                 // document.getElementsByClassName("nav-bar")[0].getElementsByClassName("nav-bar-pc")[0].children[0].innerText = capitalizeFirstLetter(u.nickname);
             }
         apiNav.GetMyProfil(t.token);
