722607fe74d6ed9b64bfd31f8aa1f6b10d715ec2
Atduyar <atduyar@gmail.com>
2021-08-19 12:07:39 +0300

    tamam

--- js/user.js
+++ js/user.js
@@ -8,7 +8,7 @@ function setUser(u){
     document.getElementById("user-pp").children[0].src = "https://api.atduyar.com/Images/" + u.avatarUrl;
 }
 
-function getUser(userId, fixUrl=()=>{}) {
+function getUser(userName, fixUrl=()=>{}) {
     apiUser.resultFunction = (t) => {
         console.log(t);
         apiUser.resultFunction = (b) => {
@@ -18,7 +18,7 @@ function getUser(userId, fixUrl=()=>{}) {
             setUser(b);
         }
         apiUser.resultErrFunction = apiUser.resultErrFunction;
-        apiUser.GetAuth("users/getUser?id="+userId, t.token);
+        apiUser.GetAuth("users/getUserByName?name="+userName, t.token);
     }
     apiUser.resultErrFunction = (t) => {
         console.log(t);
@@ -30,12 +30,14 @@ function getUser(userId, fixUrl=()=>{}) {
 }
 
 
-var xxidTemp = new URLSearchParams(window.location.search).get('id');
-if(xxidTemp != null){
-    getUser(xxidTemp, (userNickname)=>{history.pushState({}, null, "/user.html?name=" + userNickname + "&id=" + xxidTemp)});
+var xxTemp = new URLSearchParams(window.location.search).get('name');
+if(xxTemp != null){
+    getUser(xxTemp, (userNickname)=>{history.pushState({}, null, "/user.html?name=" + xxTemp)});
+    // getUser(xxidTemp, (userNickname)=>{history.pushState({}, null, "/user.html?name=" + userNickname + "&id=" + xxidTemp)});
 }
 function setParam(param){
     var x = param.split("-");
-    getUser(x[x.length - 1], (userNickname)=>{history.pushState({}, null, "/user/" + userNickname + "-" + x[x.length - 1])});
+    getUser(x[0], (userNickname)=>{history.pushState({}, null, "/user/" + x[0])});
+    // getUser(x[x.length - 1], (userNickname)=>{history.pushState({}, null, "/user/" + userNickname + "-" + x[x.length - 1])});
     
 }
