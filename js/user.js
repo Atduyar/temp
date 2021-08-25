9ebf4b9953e99fc9aa34e1ccba15e682b59378a9
Atduyar <atduyar@gmail.com>
2021-08-25 17:21:59 +0300

    tamam

--- js/user.js
+++ js/user.js
@@ -17,7 +17,7 @@ function getUser(userName, fixUrl=()=>{}) {
             fixUrl(b.nickname);
             setUser(b);
             getUserReaded(b.id);
-            getUserBlogs(b.id);
+            //getUserBlogs(b.id);
         }
         apiUser.resultErrFunction = apiUser.resultErrFunction;
         apiUser.GetAuth("users/getUserByName?name="+userName, t.token);
