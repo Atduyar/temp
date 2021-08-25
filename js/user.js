3b4c3fc20e02827724bab0e829bf39e227442eea
Atduyar <atduyar@gmail.com>
2021-08-25 13:35:58 +0300

    tamam

--- js/user.js
+++ js/user.js
@@ -16,6 +16,7 @@ function getUser(userName, fixUrl=()=>{}) {
             user = b;
             fixUrl(b.nickname);
             setUser(b);
+            getUserReaded(userReadedPageId);
         }
         apiUser.resultErrFunction = apiUser.resultErrFunction;
         apiUser.GetAuth("users/getUserByName?name="+userName, t.token);
@@ -24,7 +25,7 @@ function getUser(userName, fixUrl=()=>{}) {
         console.log(t);
     }
     apiUser.resultUnAuthFunction = (t) => {
-        console.log(t);
+        console.log(t);//ulasılamaz penceresi yap
     }
     ApiAuth.GetToken(apiUser)
 }
@@ -33,7 +34,7 @@ var userPage = document.getElementById("user-body-page");
 var userPageLine = document.getElementById("user-body-nav-line");
 function changeUBPage(x){
     userPage.style = "transform: translateX(calc( (-100% / 3) * "+x+" ));";
-    userPageLine.style = "padding-left: calc("+(x)+" * 20vw + var(--nav-line-fix));!important";
+    userPageLine.style = "padding-left: calc("+(x)+" * 20vw + var(--nav-line-fix    ));!important";
     // userPageLine.style = "padding: 0 calc("+(2-x)+" var(--nav-size)) 0 calc("+(x)+" * var(--nav-size));!important";
     // console.log("padding: 0 "+(4-x*2)+"0vw 0 "+x*2+"0vw;!important");
 }
@@ -47,3 +48,27 @@ function setParam(param){
     getUser(x[0], (userNickname)=>{history.pushState({}, null, "/user/" + x[0])});
     
 }
+
+
+function setUserReaded(b){
+    console.log(b);
+}
+
+var userReadedPageId = 1;
+function getUserReaded(userId) {
+    apiUser.resultFunction = (t) => {
+        console.log(t);
+        apiUser.resultFunction = (b) => {
+            setUserReaded(b);
+        }
+        apiUser.resultErrFunction = apiUser.resultErrFunction;
+        apiUser.GetAuth("users/getUserReaded?id="+userId+"pageId="+userReadedPageId+"&pageSize=42", t.token);
+    }
+    apiUser.resultErrFunction = (t) => {
+        console.log(t);
+    }
+    apiUser.resultUnAuthFunction = (t) => {
+        console.log(t);
+    }
+    ApiAuth.GetToken(apiUser)
+}//https://api.atduyar.com/api/users/getUserReaded?id=1&pageId=1&pageSize=3
