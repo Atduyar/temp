a9dfd2e8bd19fb94fb2c25495596423d01c17c9a
Atduyar <atduyar@gmail.com>
2021-08-25 17:02:15 +0300

    tamam

--- js/user.js
+++ js/user.js
@@ -83,7 +83,6 @@ function setUserReaded(b){
             </div>
         </div>`
     }
-    
 }
 
 var userReadedPageId = 1;
@@ -104,3 +103,60 @@ function getUserReaded(userId) {
     }
     ApiAuth.GetToken(apiUser)
 }//https://api.atduyar.com/api/users/getUserReaded?id=1&pageId=1&pageSize=3
+
+
+
+
+function setUserReaded(b){
+    console.log(b);
+    var readedPage = document.getElementById("user-body-page").children[2].children[0];
+    if(userReadedPageId == 1){
+        readedPage.innerHTML = "";
+    } 
+    userReadedPageId++;
+    for(var i = 0; i< b.length;i++){
+        readedPage.innerHTML += 
+        `<div class="${((i%18) >= 12)?"blog-list-long ":""}blog-item">
+            <a href="/blogView/${b[i].blogId}" class="a-img-blog-item" >
+                <img class="img-blog-item" src="${b[i].blogTitlePhotoUrl}" loading="lazy" alt="${b[i].blogTitle}">
+            </a>
+            <div class='blog-content'>
+                <p class="p-publish-title only-long">
+                    ${b[i].blogDate}
+                </p>
+                <div class="blog-des-group">
+                    <p class="text-color p-baslik-blog-item c-p">${b[i].blogTitle}</p>
+                    <p class="p-aciklama-blog-item">${(b[i].blogSummary.length > 250) ? b[i].blogSummary.substring(0, 225) + "...":b    [i].blogSummary }</p>
+                </div>
+                <div class="blog-item-details">
+                    <div class="kategori-bar-blog-item">
+                        <div class="blog-item-author-info">
+                            <a href="/user/${b[i].authorName}" class="inactive-blackbg c-p td-n">${b[i].authorName}</a>
+                        </div>
+                        <a href="/category/Politika" class="p-kategori-black-blog-item inactive-blackbg c-p td-n">Politika</a>
+                        <a href="/category/Elestiri" class="p-kategori-black-blog-item inactive-blackbg c-p td-n">Eleştiri</a>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
+}
+
+var userBlogPageId = 1;
+function getUserReaded(userId) {
+    apiUser.resultFunction = (t) => {
+        console.log(t);
+        apiUser.resultFunction = (b) => {
+            setUserReaded(b);
+        }
+        apiUser.resultErrFunction = apiUser.resultErrFunction;
+        apiUser.GetAuth("users/getUserBlog?id="+userId+"&pageId="+userBlogPageId+"&pageSize=42", t.token);
+    }
+    apiUser.resultErrFunction = (t) => {
+        console.log(t);
+    }
+    apiUser.resultUnAuthFunction = (t) => {
+        console.log(t);
+    }
+    ApiAuth.GetToken(apiUser)
+}//https://api.atduyar.com/api/users/getUserBlog?id=1&pageId=1&pageSize=3
