614d8200abf1eb02ed2ee4591f15e22c330b8562
Atduyar <atduyar@gmail.com>
2021-08-25 14:46:36 +0300

    tamam

--- js/user.js
+++ js/user.js
@@ -57,30 +57,33 @@ function setUserReaded(b){
         readedPage.innerHTML = "";
     } 
     userReadedPageId++;
-    readedPage.innerHTML += 
-    `<div class="${((i%18) >= 12)?"blog-list-long ":""}blog-item">
-    <a href="/blogView/${b[i].blogId}" class="a-img-blog-item" >
-        <img class="img-blog-item" src="${b[i].blogTitlePhotoUrl}" loading="lazy" alt="${b[i].blogTitle}">
-    </a>
-    <div class='blog-content'>
-        <p class="p-publish-title only-long">
-            ${b[i].blogDate}
-        </p>
-        <div class="blog-des-group">
-            <p class="text-color p-baslik-blog-item c-p">${b[i].blogTitle}</p>
-            <p class="p-aciklama-blog-item">${(b[i].blogSummary.length > 250) ? b[i].blogSummary.substring(0, 225) + "...":b[i].blogSummary }</p>
-        </div>
-        <div class="blog-item-details">
-            <div class="kategori-bar-blog-item">
-                <div class="blog-item-author-info">
-                    <a href="/user/${b[i].authorName}" class="inactive-blackbg c-p td-n">${b[i].authorName}</a>
+    for(var i = 0; i< b.length;i++){
+        readedPage.innerHTML += 
+        `<div class="${((i%18) >= 12)?"blog-list-long ":""}blog-item">
+            <a href="/blogView/${b[i].blogId}" class="a-img-blog-item" >
+                <img class="img-blog-item" src="${b[i].blogTitlePhotoUrl}" loading="lazy" alt="${b[i].blogTitle}">
+            </a>
+            <div class='blog-content'>
+                <p class="p-publish-title only-long">
+                    ${b[i].blogDate}
+                </p>
+                <div class="blog-des-group">
+                    <p class="text-color p-baslik-blog-item c-p">${b[i].blogTitle}</p>
+                    <p class="p-aciklama-blog-item">${(b[i].blogSummary.length > 250) ? b[i].blogSummary.substring(0, 225) + "...":b    [i].blogSummary }</p>
                 </div>
-                <a href="/category/Politika" class="p-kategori-black-blog-item inactive-blackbg c-p td-n">Politika</a>
-                <a href="/category/Elestiri" class="p-kategori-black-blog-item inactive-blackbg c-p td-n">Eleştiri</a>
+                <div class="blog-item-details">
+                    <div class="kategori-bar-blog-item">
+                        <div class="blog-item-author-info">
+                            <a href="/user/${b[i].authorName}" class="inactive-blackbg c-p td-n">${b[i].authorName}</a>
+                        </div>
+                        <a href="/category/Politika" class="p-kategori-black-blog-item inactive-blackbg c-p td-n">Politika</a>
+                        <a href="/category/Elestiri" class="p-kategori-black-blog-item inactive-blackbg c-p td-n">Eleştiri</a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</div>`
+        </div>`
+    }
+    
 }
 
 var userReadedPageId = 1;
