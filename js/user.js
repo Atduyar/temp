49bc2e4d00f3b2804c7685c22d8d6eb8636db4c9
Atduyar <atduyar@gmail.com>
2021-08-25 14:44:29 +0300

    tamam

--- js/user.js
+++ js/user.js
@@ -52,6 +52,12 @@ function setParam(param){
 
 function setUserReaded(b){
     console.log(b);
+    var readedPage = document.getElementById("user-body-page").children[0];
+    if(userReadedPageId == 1){
+        readedPage.innerHTML = "";
+    } 
+    userReadedPageId++;
+    readedPage.innerHTML += ``
 }
 
 var userReadedPageId = 1;
