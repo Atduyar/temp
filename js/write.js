36abb841a8638af61c838ec66562c80a22c23cbd
Atduyar <atduyar@gmail.com>
2021-09-16 16:31:46 +0300

    tamam

--- js/write.js
+++ js/write.js
@@ -4,24 +4,25 @@ function fixEvents(){
     // oninput="if(this.innerHTML.trim()==='<br>')this.innerHTML=''"
     var max = -1;
     for(var i = 0; i < textboxs.length; i++) {
-        if(textboxs[i].getAttribute("atdId") == null){
-            console.log(i);
-            textboxs[i].setAttribute("atdId", i);
-            textboxs[i].addEventListener('keydown', (evt) => {
-                if (evt.keyCode === 13) {
-                    textboxId = textboxs.length;
-                    
-    
-                    evt.preventDefault();
-                }
-            });
-
-        }
-        else{
+        if(textboxs[i].getAttribute("atdId") != null){
             if(max < textboxs[i].getAttribute("atdId")){
                 max = textboxs[i].getAttribute("atdId");
             }
         }
+    }
+
+    for(var i = 0; i < textboxs.length; i++) {
+        console.log(i);
+        textboxs[i].setAttribute("atdId", i);
+        textboxs[i].addEventListener('keydown', (evt) => {
+            if (evt.keyCode === 13) {
+                textboxId = textboxs.length;
+                
+
+                evt.preventDefault();
+            }
+        });
+
     }
     console.log(-1);
     textboxId = max;
