3451fd06d56245ad831e5a6b232a5c7f70d8272a
Atduyar <atduyar@gmail.com>
2021-09-16 14:28:50 +0300

    tamam

---
+++ js/write.js
@@ -0,0 +1,18 @@
+var textboxs = document.getElementsByClassName('textbox');
+var textboxId = 0;
+
+function fixEvents(){
+    // oninput="if(this.innerHTML.trim()==='<br>')this.innerHTML=''"
+    for(var i = textboxId; i < textboxs.length; i++) {
+        console.log(i);
+        textboxs[i].addEventListener('keydown', (evt) => {
+            if (evt.keyCode === 13) {
+                textboxId = textboxs.length-1;
+                
+
+                evt.preventDefault();
+            }
+        });
+    }
+    textboxId = textboxs.length-1;
+}
