9744e72d6324cadbfb03837c06f3781340038748
Atduyar <atduyar@gmail.com>
2021-09-29 21:56:56 +0300

    tamam

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
     $x = explode("/", $_SERVER["REQUEST_URI"]);
     // print_r($x);
-    if(str_contains($x[1], ".xml")){
-        $y = __DIR__ . '/' . $x[1] ;
-    }
-    elseif (count($x) > 2){
+    if (count($x) > 2){
         $y = __DIR__ . '/' . $x[1] . '.html';
         require $y;
         echo '<script type="text/javascript">',
              'setParam("' . $x[2] . '");',
              '</script>';
+    }
+    elseif(str_contains($x[1], ".xml")){
+        $y = __DIR__ . '/' . $x[1] ;
     }
     elseif(count($x) > 1 and $x[1] != ""){
         $y = __DIR__ . '/' . $x[1] . '.html';
