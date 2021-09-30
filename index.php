73fc06bda4b9f06edde3af0789936cd80b7dac4b
Atduyar <atduyar@gmail.com>
2021-09-30 18:27:18 +0300

    tamam

--- index.php
+++ index.php
@@ -5,29 +5,8 @@
         if($x[1] == "blogView"){
 
             $y = __DIR__ . '/' . $x[1] . '.html';
-            echo '<!DOCTYPE html>',
-                 '<html lang="tr">',
-                 '<head>',
-                 '<meta name="description" content="Test ile bizimle bilimi kesfedin.">',
-                 '<meta property="og:title" content="Test">',
-                 '<meta property="og:description" content="Test ile bizimle bilimi kesfedin.">',
-                 '<meta property="og:image" content="https://api.atduyar.com/ConstImage/titleBanner.png">',
-                 '<meta property="og:site_name" content="Test">',
-                 '<meta name="twitter:title" content="Test">',
-                 '<meta name="twitter:description" content="Test ile bizimle bilimi kesfedin.">',
-                 '<meta name="twitter:image" content="https://api.atduyar.com/ConstImage/titleBanner.png">',
-                 '<meta name="twitter:card" content="https://api.atduyar.com/ConstImage/titleBanner.png">';
 
-            require $y;
-            echo '<script type="text/javascript">',
-                 'console.log("blogView");',
-                 'setParam("' . $x[2] . '");',
-                 '</script>';
-        }
-        else{
-            $y = __DIR__ . '/' . $x[1] . '.html';
-
-            $content =     file_get_contents("https://api.atduyar.com/api/blogs/getBlogMeta?id=1");
+            $content = file_get_contents("https://api.atduyar.com/api/blogs/getBlogMeta?id=1");
             $result  = json_decode($content);
 
             echo '<!DOCTYPE html>',
@@ -42,6 +21,28 @@
                  '<meta name="twitter:description" content="' . $result->blogSummary . '">',
                  '<meta name="twitter:image" content="' . $result->blogTitlePhotoUrl . '">',
                  '<meta name="twitter:card" content="' . $result->blogTitlePhotoUrl . '>';
+
+            require $y;
+            echo '<script type="text/javascript">',
+                 'console.log("blogView");',
+                 'setParam("' . $x[2] . '");',
+                 '</script>';
+        }
+        else{
+            $y = __DIR__ . '/' . $x[1] . '.html';
+            
+            echo '<!DOCTYPE html>',
+                 '<html lang="tr">',
+                 '<head>',
+                 '<meta name="description" content="Evrenomi ile bizimle bilimi kesfedin.">',
+                 '<meta property="og:title" content="Evrenomi">',
+                 '<meta property="og:description" content="Evrenomi ile bizimle bilimi kesfedin.">',
+                 '<meta property="og:image" content="https://api.atduyar.com/ConstImage/titleBanner.png">',
+                 '<meta property="og:site_name" content="Evrenomi">',
+                 '<meta name="twitter:title" content="Evrenomi">',
+                 '<meta name="twitter:description" content="Evrenomi ile bizimle bilimi kesfedin.">',
+                 '<meta name="twitter:image" content="https://api.atduyar.com/ConstImage/titleBanner.png">',
+                 '<meta name="twitter:card" content="https://api.atduyar.com/ConstImage/titleBanner.png">';
 
             require $y;
             echo '<script type="text/javascript">',
