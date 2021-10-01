efc2b3b0af8215ea66ef8cbca547f908885a1f03
Atduyar <atduyar@gmail.com>
2021-10-02 00:44:57 +0300

    tamam

--- js/write.js
+++ js/write.js
@@ -42,7 +42,7 @@ function addEvent(x){
 }
 
 addElementToEnd("img");
-addElementToEnd("h1");
+addElementToEnd("h");
 addElementToEnd("p");
 fixEvents();
 var max = -1;
@@ -72,14 +72,14 @@ function findNextElement(tagName){
     tagName = tagName.toLocaleLowerCase();
     switch(tagName){
         case "a":
-        case "h1":
+        case "h":
         case "img":
         case "ımg":
         case "quote":
             tagName = "p";
             break;
         case "sep":
-            tagName = "h1";
+            tagName = "h";
             break;
         case "li":
         case "lı":
@@ -96,8 +96,8 @@ function addElement(tagName){
         case "p":
             return getDefualtElement(`<p contenteditable class="textbox" placeholder="Type something..."></p>`, "p")
             break;
-        case "h1":
-            return getDefualtElement(`<h1 contenteditable class="textbox title" placeholder="Type something..."></h1>`, "h1")
+        case "h":
+            return getDefualtElement(`<h1 contenteditable class="textbox title" placeholder="Type something..."></h1>`, "h")
             break;
         case "quote":
             return getDefualtElement(`<p contenteditable class="textbox quote" placeholder="Type something..."></p>`, "quote")
@@ -189,7 +189,7 @@ function getItemJson(item){
 
     switch(newItemType){
         case "p":
-        case "h1":
+        case "h":
         case "li":
         case "lı":
             newItemData = item.getElementsByClassName("textbox")[0].textContent;
@@ -269,7 +269,7 @@ function getDefualtElement(htl, itemTag){
             </div>
             <div class="menu">
                 <ul>
-                    <li onclick="addElementToNext(this.parentElement.parentElement.parentElement.parentElement, 'h1')">
+                    <li onclick="addElementToNext(this.parentElement.parentElement.parentElement.parentElement, 'h')">
                         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 190 190" style="enable-background:new 0 0 190 190;" xml:space="preserve"><path id="XMLID_27_" d="M175,0H15C6.716,0,0,6.716,0,15v38.99c0,8.284,6.716,15,15,15c8.284,0,15-6.716,15-15V30h50v130H56.836  c-8.284,0-15,6.716-15,15s6.716,15,15,15h76.328c8.284,0,15-6.716,15-15s-6.716-15-15-15H110V30h50v23.99c0,8.284,6.716,15,15,15  c8.284,0,15-6.716,15-15V15C190,6.716,183.284,0,175,0z"/>
                         </svg>
                     </li>
